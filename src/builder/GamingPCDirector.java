125bc366c6ecc85ff3701f5c201e9a71a04289ed
Batyrkan <batyrkhanuvaev@gmail.com>
2026-09-12 13:00:32 +0500

    added Director component

---
+++ src/builder/GamingPCDirector.java
@@ -0,0 +1,12 @@
+package builder;
+
+public class GamingPCDirector {
+    public GamingPC construct(GamingPCBuilder builder) {
+        return builder.setCpu("AMD Ryzen 7 7800X3D")
+                .setGpu("NVIDIA RTX 5070")
+                .setRam("32GB")
+                .setStorage("2TB SSD")
+                .setPowerSupply("850W")
+                .build();
+    }
+}
